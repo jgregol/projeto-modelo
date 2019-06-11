2137c435516d7ed007bdeaff537bd00f43acb77c
jgregol <jgregol@gmail.com>
2019-06-10 23:01:19 -0300

    Initial commit

---
+++ src/test/java/br/com/apex/modelo/ProjetoModeloApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.apex.modelo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetoModeloApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
