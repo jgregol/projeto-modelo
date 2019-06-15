eda2c9e32fc2e55f8ce554690ae1cb701566e726
jgregol <jgregol@gmail.com>
2019-06-15 17:07:14 -0300

    Correcoes

--- src/main/java/br/com/apex/modelo/service/DepartamentoService.java
+++ src/main/java/br/com/apex/modelo/service/DepartamentoService.java
@@ -4,18 +4,18 @@ import java.util.List;
 
 import br.com.apex.modelo.domain.Departamento;
 
+
 public interface DepartamentoService {
 
-	void salvar(Departamento departamento);
+    void salvar(Departamento departamento);
 
-	void editar(Departamento departamento);
+    void editar(Departamento departamento);
 
-	void excluir(Long id);
+    void excluir(Long id);
 
-	Departamento buscarPorId(Long id);
-
-	List<Departamento> buscarTodos();
+    Departamento buscarPorId(Long id);
+    
+    List<Departamento> buscarTodos();
 
 	boolean departamentoTemCargos(Long id);
-
 }
